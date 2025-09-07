e5370e69556e32e5a3fb164030cc34ba3d20829e
Nisad Islam <nisadnn2001@gmail.com>
2025-09-07 14:11:10 +0600

    html css done

---
+++ script.js
@@ -0,0 +1,83 @@
+// heart funtionalities
+
+document.getElementById('heart-icon1').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+
+document.getElementById('heart-icon2').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+
+document.getElementById('heart-icon3').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+document.getElementById('heart-icon4').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+document.getElementById('heart-icon5').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+document.getElementById('heart-icon6').addEventListener('click', function() {
+    const availableHeartsEl = document.getElementById('available-hearts');
+    let availableHearts = parseInt(availableHeartsEl.innerText); 
+
+    availableHearts += 1; 
+
+    availableHeartsEl.innerText = availableHearts; 
+
+});
+
+
+
+// call button funtionalities
+
+
+document.getElementById('call1').addEventListener('click', function(){
+    alert('you are calling National Emergency')
+})
+document.getElementById('call2').addEventListener('click', function(){
+    alert('you are calling police ')
+})
+document.getElementById('call3').addEventListener('click', function(){
+    alert('you are calling Fire Service')
+})
+document.getElementById('call4').addEventListener('click', function(){
+    alert('you are calling Ambulance')
+})
+document.getElementById('call5').addEventListener('click', function(){
+    alert('you are calling  Women & Child Helpline')
+})
+document.getElementById('call6').addEventListener('click', function(){
+    alert('you are calling Anti-Corruption')
+})
+
